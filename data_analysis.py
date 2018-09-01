8a287731fd2ff1d2516f7e446ea4c0eeac91b6a0
Yash <yash.parekh223@gmail.com>
2018-09-01 15:51:30 -0600

    Further math analysis

--- data_analysis.py
+++ data_analysis.py
@@ -1,37 +1,20 @@
+#Data Analysis 					#
+#Author: Yash Parekh			#
+#Langauge: python 				#
+#Run: python3 data_analysis.py  #
+#################################
+import sys
+import fractions
+import math
 
-#Title: average()
-#Description: computes average of set of numbers
-def average(numbers):
-	average = 0
-	count = 0
-	for number in numbers:
-		count += 1
-		average += number
-	average = average/count
-	
-	return average
-
-'''
-def stddev(numbers):
-	average = average(numbers)
-	stddev = 0
-	count = 0
-	for x in numbers:
-		stddev = stddev += (x-average)**2
-		count += 1
-
-	stddev = stddev/count
-
-	return stddev
-'''
 def main():
 	print('Welcome to the data analysis calculator')
 
 	command = ''
 	while (command != 'q'):
-		command = input('What are you trying to do? Select option.\n1. Sum\n2. Difference\n3. Multiply\n4. Average\n5. Standard Deviation\n6. Greatest Common Factor\n7. Raise to Power\n8. Is Prime?\n9. Least Common Multiple * Under Construction*\n10. \nq. Quit\nEnter option: ')
+		command = input('What are you trying to do? Select option.\n1. Sum\n2. Difference\n3. Multiply\n4. Average\n5. Standard Deviation\n6. Greatest Common Factor\n7. Raise to Power\n8. Is Prime?\n9. Least Common Multiple * Under Construction*\n10. Smallest Number Divisble by 1 to N\n11. Factorial Digit Sum\nq. Quit\nEnter option: ')
 		if command == '1':
-			print('Sum - this will return the absolute value of the sum of the numbers.')
+			print('\nSum - this will return the absolute value of the sum of the numbers.')
 			Sum =  0
 			num_of_numbers = int(input('How many numbers do you want to add?\t'))
 
@@ -46,7 +29,7 @@ def main():
 
 
 		if command == '2':
-			print('Difference - this will return the absolute value of the difference between the two numbers.')
+			print('\nDifference - this will return the absolute value of the difference between the two numbers.')
 			first_number = int(input('What is the first number?\t'))
 			second_number = int(input('What is the second number?\t'))
 			Difference = first_number - second_number
@@ -56,8 +39,8 @@ def main():
 			#return Difference
 
 		if command == '3':
-			print('Multiply - this will return the value of the numbers multiplied')
-			print('Input numbers to multiply one at a time, followed by the * key')
+			print('\nMultiply - this will return the value of the numbers multiplied')
+			#print('Input numbers to multiply one at a time, followed by the * key')
 
 			total = 1
 			num_elements_to_multiply = int(input('Enter number of numbers to multiply: '))
@@ -65,11 +48,12 @@ def main():
 				number_to_multiply = int(input('Enter number: '))
 				total *= number_to_multiply
 				num_elements_to_multiply -= 1
-			print(total + '\n')
+			print(total)
+			print('\n')
 
 
 		if command == '4':
-			print('Average - this will return the average of the numbers.')
+			print('\nAverage - this will return the average of the numbers.')
 			Sum = 0
 			Average = 0
 			num_of_numbers = int(input('How many numbers do you want to take the average of?\t'))
@@ -85,7 +69,7 @@ def main():
 
 
 		if command == '5':
-			print('Standard Deviaion - this will return the standard deviation of the numbers.')
+			print('\nStandard Deviaion - this will return the standard deviation of the numbers.')
 
 			#1. calculate average
 			Sum = 0
@@ -111,27 +95,28 @@ def main():
 			stddev = stddev/count
 
 		if command == '6':
-			print('Greatest Common Factor - this will return the GCF of the numbers.')
+			print('\nGreatest Common Factor - this will return the GCF of the numbers.')
 			print('Currently this feature works with only two numbers')
 
 			num1 = int(input('What is the first number?'))
 			num2 = int(input('What is the second number?'))
 
-			
 			if(num1 > num2):
-				maxfactor = num2
+				gcf = num2
 			else:
-				maxfactor = num1
-			gcf = 1
-			for i in range(1,maxfactor):
-				if(num1%i==0 and num2%i==0):
-					gcf = i
+				gcf = num1
+			while gcf > 1:
+				if num1 % gcf == 0 and num2 % gcf == 0:
+					break
+				gcf -= 1
 
-			print(gcf + '\n')
+
+			print(gcf)
+			print('\n')
 			#return gcf
 
 		if command == '7':
-			print('Raise to power - this will return result of base to power.')
+			print('\nRaise to power - this will return result of base to power.')
 			num1 = int(input('Enter base: '))
 			num2 = int(input('Enter power: '))
 
@@ -145,7 +130,7 @@ def main():
 			print(result)
 
 		if command == '8':
-			print('Check if a number is prime.')
+			print('\nCheck if a number is prime.')
 			number = int(input('Enter test number: '))
 
 			if(number == 1 or number == 2):
@@ -161,24 +146,52 @@ def main():
 		
 		# NOT DONE
 		if command == '9':
-			print('Find Least Common Multiple')
+			print('\nFind Least Common Multiple of two numbers')
 			num1 = int(input('Enter first number: '))
 			num2 = int(input('Enter second number: '))
+			max_poss_LCM = num1 * num2
 
-			if(num1 > num2):
-				LCM = num1
-			else:
-				LCM = num2
-
-			while (num1 % LCM != 0 and num2 % LCM != 0):
-				LCM -= 1
+			LCM = 2
+			for i in range(2, max_poss_LCM):
+				if(num1 % i == 0 and num2 % i == 0):
+					LCM = i
 
 			print(LCM)
+
+		#Smallest multiple: smallest positive number that is evenly divisible by all numbers 1 to N
+		if command == '10':
+			print('\nFind smallest multiple divisible by 1 to N')
+			num = int(input('Enter number: '))
+			ans = 1
+			for i in range(1, num+1):
+				ans = (ans*i)/fractions.gcd(ans, i)
+			print(ans)
+
+		#Project euler 20: Factorial digital sum
+		#find sum of digits in n!
+		if command == '11':
+			print('\nFind sum of digits in n!')
+			num = int(input('Enter number: '))
+			fact_num = math.factorial(num)
+			print(str(num)+'! = ' + str(fact_num))
+
+			sum_fact = 0
+			str_num = str(num)
+			str_fact_num = str(fact_num)
+			for i in range(0,len(str_fact_num)):
+				num_to_add = int(str_fact_num[i])
+				sum_fact += num_to_add
+
+			print('Sum of digits in ' + str_num + '! = ' + str(sum_fact))
 
 
 
 		if command == 'q':
-				print('Thank you for using the data analysis calculator.')
+			print('Thank you for using the data analysis calculator.')
+
+		else:
+			print('\nUnknown command')
+			command = 'reset'
 
 		#else:
 			#print('Unknown command, please try again')
